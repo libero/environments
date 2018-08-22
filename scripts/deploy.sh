d65770160062c6172eaa95b739bf5a39972792c0
Giorgio Sironi <g.sironi@elifesciences.org>
2018-08-22 14:23:02 +0100

    Accept shellcheck suggestions on quoting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ fi
 
 environment_name="$1"
 hostname="${environment_name}.libero.pub"
-ssh $hostname /usr/local/bin/deploy browser=$(scripts/latest-revision.sh git@github.com:libero/browser)
+ssh "$hostname" /usr/local/bin/deploy browser="$(scripts/latest-revision.sh git@github.com:libero/browser)"
