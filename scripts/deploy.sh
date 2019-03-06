8d561bb8ead5789490054a811a2fe18128d1c2df
Giorgio Sironi <g.sironi@elifesciences.org>
2019-03-06 17:32:34 +0000

    Make the number of application scalable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,16 @@ key="$2"
 public_port=80
 
 scp -o StrictHostKeyChecking=no -i "$key" scripts/remote-deploy.sh "$ssh_hostname":/tmp/remote-deploy.sh
-revision_browser=$(scripts/latest-revision.sh https://github.com/libero/browser.git)
-revision_dummy_api=$(scripts/latest-revision.sh https://github.com/libero/dummy-api.git)
-revision_pattern_library=$(scripts/latest-revision.sh https://github.com/libero/pattern-library.git)
-ssh -o StrictHostKeyChecking=no -i "$key" "$ssh_hostname" PUBLIC_PORT=${public_port} REVISION_BROWSER="$revision_browser" REVISION_DUMMY_API="$revision_dummy_api" REVISION_PATTERN_LIBRARY="$revision_pattern_library" /tmp/remote-deploy.sh
+
+# list of applications
+declare -a applications=(browser dummy-api pattern-library)
+# environment variables string
+declare environment
+for application in "${applications[@]}"
+do
+    revision=$(scripts/latest-revision.sh "https://github.com/libero/${application}.git")
+    environment_variable_name="REVISION_$(echo $application | tr a-z A-Z | tr - _)"
+    environment="${environment} $environment_variable_name=${revision}"
+done
+
+ssh -o StrictHostKeyChecking=no -i "$key" "$ssh_hostname" PUBLIC_PORT=${public_port} $environment /tmp/remote-deploy.sh
