99ddac7dd90ad5670b572f636582e54f1b4c8035
Giorgio Sironi <g.sironi@elifesciences.org>
2019-03-07 11:52:39 +0000

    Use tr with alphabets as the shellcheck suggestion breaks horribly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,8 @@ declare environment
 for application in "${applications[@]}"
 do
     revision=$(scripts/latest-revision.sh "https://github.com/libero/${application}.git")
-    environment_variable_name="REVISION_$(echo $application | tr a-z A-Z | tr - _)"
+    # shellcheck disable=SC2018 disable=SC2019
+    environment_variable_name="REVISION_$(echo "$application" | tr a-z A-Z | tr - _)"
     environment="${environment} $environment_variable_name=${revision}"
 done
 
